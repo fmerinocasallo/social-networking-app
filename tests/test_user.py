9dc5f7734ccc6c8a9c989063b053b1b5a94c2a3a
Francisco Merino-Casallo <francisco.merinocasallo+github@gmail.com>
2025-04-17 13:30:39 +0200

    test: add failing test for User equality
    - Add test_user_eq to compare users by name
    - Test fails as expected in TDD red stage

--- tests/test_user.py
+++ tests/test_user.py
@@ -7,3 +7,9 @@ def test_user_init():
     user = User('Alice')
     assert user.get_name() == 'Alice', "User name should be set correctly"
     assert not user.has_posts(), "User should start with no posts"
+
+def test_user_eq():
+    """Checks that the user is equal to another user with the same name."""
+    user1 = User('Alice')
+    user2 = User('Alice')
+    assert user1 == user2, "Users with same name should be equal"
